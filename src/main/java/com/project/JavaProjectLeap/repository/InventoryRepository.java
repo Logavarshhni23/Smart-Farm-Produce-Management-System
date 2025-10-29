0a4b091454ee9ddf860c9fdd57309c831868dd9f
Logavarshhni23 <logavarshhnis2007@gmail.com>
2025-10-29 12:40:18 +0530

    added InventoryService.java for crop creatin,updation and deletion

--- src/main/java/com/project/JavaProjectLeap/repository/InventoryRepository.java
+++ src/main/java/com/project/JavaProjectLeap/repository/InventoryRepository.java
@@ -3,8 +3,9 @@ package com.project.JavaProjectLeap.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.project.JavaProjectLeap.entity.Inventory;
-
+import java.util.List;
+import com.project.JavaProjectLeap.entity.User;
 @Repository
 public interface InventoryRepository extends JpaRepository<Inventory, Long>{
-    
+    List<Inventory> findByUser(User user);
 }
---
+++ src/main/java/com/project/JavaProjectLeap/service/InventoryService.java
@@ -0,0 +1,43 @@
+package com.project.JavaProjectLeap.service;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.project.JavaProjectLeap.repository.InventoryRepository;
+import com.project.JavaProjectLeap.entity.Inventory;
+import com.project.JavaProjectLeap.entity.User;
+
+@Service
+public class InventoryService {
+
+    @Autowired
+    private InventoryRepository inventoryRepository;
+
+    public Inventory createInventory(Inventory inventory){
+        return inventoryRepository.save(inventory);
+    }
+
+    public List<Inventory> getInventoryByUser(User user){
+        return inventoryRepository.findByUser(user);
+    }
+
+    public List<Inventory> getAllInventory(){
+        return inventoryRepository.findAll();
+    }
+
+    public Inventory updateInventory(Long id, Inventory updatedInventory){
+        Inventory inventory = inventoryRepository.findById(id).orElse(null);
+        if(inventory != null){
+            inventory.setCropName(updatedInventory.getCropName());
+            inventory.setCropType(updatedInventory.getCropType());
+            inventory.setQuantity(updatedInventory.getQuantity());
+            return inventoryRepository.save(inventory);
+        }
+        return null;
+    }
+
+    public void deleteInventory(Long id){
+        inventoryRepository.deleteById(id);
+    }
+    
+}
