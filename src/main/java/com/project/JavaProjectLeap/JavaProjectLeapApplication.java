77a55830e9b75326acf419747ab4915302fd6383
Logavarshhni23 <logavarshhnis2007@gmail.com>
2025-10-29 09:37:07 +0530

    initial commit

---
+++ src/main/java/com/project/JavaProjectLeap/JavaProjectLeapApplication.java
@@ -0,0 +1,13 @@
+package com.project.JavaProjectLeap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProjectLeapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaProjectLeapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/JavaProjectLeap/JavaProjectLeapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.JavaProjectLeap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaProjectLeapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
