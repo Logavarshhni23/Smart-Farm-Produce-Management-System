ef286fde2e0372f50e8677506fabfed57a942b5d
Logavarshhni23 <logavarshhnis2007@gmail.com>
2025-10-29 12:16:47 +0530

    added UserService.java to create new user,get user by role and id

--- src/main/java/com/project/JavaProjectLeap/repository/UserRepository.java
+++ src/main/java/com/project/JavaProjectLeap/repository/UserRepository.java
@@ -3,8 +3,10 @@ package com.project.JavaProjectLeap.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.project.JavaProjectLeap.entity.User;
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
-    
+    List<User> findByRole(String role);
+    User findByEmail(String email);
 }
---
+++ src/main/java/com/project/JavaProjectLeap/service/UserService.java
@@ -0,0 +1,38 @@
+package com.project.JavaProjectLeap.service;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.project.JavaProjectLeap.repository.UserRepository;
+import com.project.JavaProjectLeap.entity.User;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User createUser(User user){
+        return userRepository.save(user);
+    }
+
+    public List<User> getUserByRole(String role){
+        return userRepository.findByRole(role);
+    }
+    
+    public User getUserById(Long id){
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public User login(String email, String password){
+        User user = userRepository.findByEmail(email);
+        if(user!=null && user.getPassword().equals(password)){
+            return user;
+        }
+        return null;
+    }
+
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+}
